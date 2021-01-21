1a6e6b172e20b2e759e3748fb70e58bdec21be9b
ahtisham521 <ahtisham.mlk123@gmail.com>
2021-01-21 21:52:47 +0500

    image input file fully updated

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -3,35 +3,62 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as path;
-import 'package:path_provider/path_provider.dart' as sysPaths;
+import 'package:path_provider/path_provider.dart' as syspath;
 
 class ImageInput extends StatefulWidget {
-  final Function onSelectImage;
-  ImageInput(this.onSelectImage);
+  final Function _selectImage;
+
+  const ImageInput(this._selectImage);
+
   @override
   _ImageInputState createState() => _ImageInputState();
 }
 
 class _ImageInputState extends State<ImageInput> {
-  File _storedImage;
-  final picker = ImagePicker();
-  Future<void> _getImage() async {
-    final pickedImage = await picker.getImage(
+  File _showedImage;
+
+  Future<void> _takePicture() async {
+    final pickedFile = await ImagePicker().getImage(
       source: ImageSource.camera,
       maxWidth: 600,
     );
-    setState(() {
-      _storedImage = File(pickedImage.path);
-    });
-    try {
-      final appDir = await sysPaths.getApplicationDocumentsDirectory();
-      final fileName = path.basename(pickedImage.path);
-      final savedImage =
-          File(pickedImage.path).copy('${appDir.path}/$fileName');
-      widget.onSelectImage(savedImage);
-    } on Exception catch (e) {
-      print(e);
+
+    if (pickedFile == null) {
+      return;
     }
+
+    setState(() {
+      _showedImage = File(pickedFile.path);
+    });
+
+    final appDir = await syspath.getApplicationDocumentsDirectory();
+    final fileName = path.basename(pickedFile.path);
+    final savedImage =
+        await File(pickedFile.path).copy('${appDir.path}/$fileName');
+
+    widget._selectImage(savedImage);
+  }
+
+  Future<void> _openImageFromGallery() async {
+    final pickedFile = await ImagePicker().getImage(
+      source: ImageSource.gallery,
+      maxWidth: 600,
+    );
+
+    if (pickedFile == null) {
+      return;
+    }
+
+    setState(() {
+      _showedImage = File(pickedFile.path);
+    });
+
+    final appDir = await syspath.getApplicationDocumentsDirectory();
+    final fileName = path.basename(pickedFile.path);
+    final savedImage =
+        await File(pickedFile.path).copy('${appDir.path}/$fileName');
+
+    widget._selectImage(savedImage);
   }
 
   @override
@@ -39,19 +66,16 @@ class _ImageInputState extends State<ImageInput> {
     return Row(
       children: [
         Container(
-          width: 150,
-          height: 150,
+          width: 200,
+          height: 200,
           decoration: BoxDecoration(
-            border: Border.all(
-              width: 1,
-              color: Colors.grey,
-            ),
+            border: Border.all(width: 1, color: Colors.grey),
           ),
-          child: _storedImage != null
+          child: _showedImage != null
               ? Image.file(
-                  _storedImage,
-                  fit: BoxFit.cover,
+                  _showedImage,
                   width: double.infinity,
+                  fit: BoxFit.cover,
                 )
               : Text(
                   'No Image Selected',
@@ -63,43 +87,22 @@ class _ImageInputState extends State<ImageInput> {
           width: 10,
         ),
         Expanded(
-          child: FlatButton.icon(
-              icon: Icon(Icons.camera),
-              label: Text(
-                'Take Picture',
+          child: Column(
+            children: [
+              FlatButton.icon(
+                icon: Icon(Icons.camera_alt),
+                label: Text('Take Picture'),
+                textColor: Theme.of(context).primaryColor,
+                onPressed: _takePicture,
               ),
-              color: Theme.of(context).primaryColor,
-              onPressed: () {
-                return showDialog(
-                  context: context,
-                  builder: (ctx) => AlertDialog(
-                    title: Text(
-                      'Alert',
-                    ),
-                    content: Text(
-                      'you want take image from Camera or Gallery ?',
-                    ),
-                    actions: [
-                      FlatButton.icon(
-                        icon: Icon(Icons.camera),
-                        onPressed: () {
-                          _getImage();
-                          Navigator.of(context, rootNavigator: true).pop();
-                        },
-                        label: Text('Camera'),
-                      ),
-                      // FlatButton.icon(
-                      //   icon: Icon(Icons.photo_camera_back),
-                      //   onPressed: () {
-                      //     _getImage(ImageSource.gallery);
-                      //     Navigator.of(context, rootNavigator: true).pop();
-                      //   },
-                      //   label: Text('Gallery'),
-                      // ),
-                    ],
-                  ),
-                );
-              }),
+              FlatButton.icon(
+                icon: Icon(Icons.image),
+                label: Text('From Gallery'),
+                textColor: Theme.of(context).primaryColor,
+                onPressed: _openImageFromGallery,
+              ),
+            ],
+          ),
         ),
       ],
     );
